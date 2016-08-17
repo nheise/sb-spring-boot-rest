c4d10608bca33a83663ba99ee80d64bcedae02db
nheise <niklas.heise@ratepay.com>
2016-08-17 10:42:40 +0200

    little clean up

--- src/main/java/de/heisemedia/gateway/runtime/ApplicationStarter.java
+++ src/main/java/de/heisemedia/gateway/runtime/ApplicationStarter.java
@@ -4,7 +4,6 @@ package de.heisemedia.gateway.runtime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
 
 
 @SpringBootApplication
